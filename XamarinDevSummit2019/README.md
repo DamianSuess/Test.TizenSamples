e92f418d5f0a86c0926797e016539340cfd48a83
Damian Suess <djsuess@gmail.com>
2021-01-04 12:03:30 -0500

    Added stripped copy of XDSummit 2019

---
+++ XamarinDevSummit2019/README.md
@@ -0,0 +1,13 @@
+# About
+The following example is a copy of the _xds2019-tizen-net_ repo
+
+Folders contain samples of pulling sensor info and a moneky watch face
+
+```
+Xamarin.Essentials Tizen backend is not yet released.
+Therefore, We prepared nuget package file for samples.
+Please reference Xamarin.Essentials.1.3.0.nupkg.
+```
+
+## Reference
+* [Xamarin Dev Summit 2019 - Tizen .NET](https://github.com/rookiejava/xds2019-tizen-net/)
---
+++ XamarinDevSummit2019/WatchFaceXamMonkey/WatchFaceXamMonkey/WatchViewModel.cs
@@ -0,0 +1,92 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace WatchFaceXamMonkey
+{
+  public class WatchViewModel : INotifyPropertyChanged
+  {
+    private string _date;
+    private string _time;
+    private FunAnimationView _animationView;
+    private WatchView _watchView;
+
+    public WatchViewModel(WatchView view)
+    {
+      _watchView = view;
+    }
+
+    public string Date
+    {
+      get => _date;
+      set
+      {
+        if (_date == value) return;
+        _date = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string Time
+    {
+      get => _time;
+      set
+      {
+        if (_time == value) return;
+        _time = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public FunAnimationView FunAnimationView
+    {
+      get => _animationView;
+      set
+      {
+        _animationView = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public Command ShowLottieAnimationCommand => new Command(ShowLottieAnimation);
+
+    public Command AnimateMonkeyCommand => new Command(AnimateMonkey);
+
+    public Command AnimationPlaybackFinishedCommand => new Command(AnimationPlaybackFinished);
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void ShowLottieAnimation()
+    {
+      var animationView = new FunAnimationView();
+      FunAnimationView = animationView;
+      FunAnimationView.LView.OnFinish += (s, e) =>
+      {
+        animationView.LView.Stop();
+        animationView.LView.IsVisible = false;
+        animationView = null;
+      };
+    }
+
+    private void AnimateMonkey()
+    {
+      var parentAnimation = new Animation();
+      var scaleUpAnimation = new Animation(v => _watchView.Monkey.Scale = v, 1, 2, Easing.SpringIn);
+      var rotateAnimation = new Animation(v => _watchView.Monkey.Rotation = v, 0, 360);
+      var scaleDownAnimation = new Animation(v => _watchView.Monkey.Scale = v, 2, 1, Easing.SpringOut);
+      parentAnimation.Add(0, 0.5, scaleUpAnimation);
+      parentAnimation.Add(0, 1, rotateAnimation);
+      parentAnimation.Add(0.5, 1, scaleDownAnimation);
+      parentAnimation.Commit(_watchView, "MultiAnimation", 16, 3000, null);
+    }
+
+    private void AnimationPlaybackFinished()
+    {
+    }
+  }
+}
